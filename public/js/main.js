9e91f02e80958d1b31871c1a08b978da23a83040
Alejandro Guerrero <brayhan_7@hotmail.com>
2018-03-16 20:37:36 -0600

    minima 01|.02
    Renderear Vistas

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
 	console.log('main.js loaded');
 
 	window.collections.articles = new Puls3.Collections.Articles();
+	
+	window.collections.articles.on('add',function(miModelo){
+		console.log('se ha agregado ',miModelo.toJSON());
+	
+	});
+	
+	
+	
 	window.collections.articles.fetch();
 });
